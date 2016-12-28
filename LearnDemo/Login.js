201df1ab8b5c4f8ad25519dccf9dfefe56cde9d0
马卿 <admin@maiqngdeMac-mini.local>
2016-12-28 09:31:23 +0800

    login

--- LearnDemo/Login.js
+++ LearnDemo/Login.js
@@ -10,6 +10,8 @@ import {
   TextInput,
   Dimensions,
   PixelRatio,
+  Button,
+  TouchableHighlight,
 } from 'react-native';
 //获取屏幕的尺寸
 var screen_width = Dimensions.get('window').width;
@@ -21,19 +23,34 @@ class Login extends Component {
       	  style={{height:80,width:80,alignSelf: 'center',borderRadius:40}}
       	  source={{uri: 'http://pic.qiantucdn.com/58pic/13/72/07/55Z58PICKka_1024.jpg'}}
       	/>
-      	<View style={[styles.input,styles.marginTop]}>
-      	<TextInput style={[styles.input,styles.center,styles.textCenter]}
+      	<TextInput style={[styles.input,styles.center,styles.textCenter,styles.marginTop]}
       				placeholder = 'QQ号/手机号/邮箱'
+               clearButtonMode = 'while-editing'
       	>
       	</TextInput>
-      	</View>
-      	<View style={[styles.input]}>
-      		<TextInput style={[styles.input,styles.center,styles.textCenter]}
+      		<TextInput style={[styles.input,styles.center,styles.textCenter,styles.lineHeight]}
       		placeholder     = '密码'
+          secureTextEntry = {true}
+          clearButtonMode = 'while-editing'
       		>
       		</TextInput>
-      	</View>
-
+          <TouchableHighlight
+            onPress={() =>{
+              console.log('login')
+            }}
+            style={[styles.loginButton,styles.center,styles.alignSelf,styles.marginTop]}
+            underlayColor='#b0e0e6'>
+            <Text style={styles.loginTextStyle}>
+              登录
+            </Text>
+          </TouchableHighlight>
+          <Button style = {styles.marginTop}
+                  onPress = {() => {
+                    console.log('assistButton clicked');
+                  }}
+                  color  = 'rgb(0,163,234)'
+                  title  = '忘记密码?'
+          ></Button>
       </View>
     );
   }
@@ -43,13 +60,15 @@ const styles = StyleSheet.create({
 container:{
 	flex:1,
 	paddingTop:64,
+  backgroundColor:'rgb(233,234,239)',
 },
 center:{
 	justifyContent: 'center',
   alignItems: 'center',
 },
-fontSize:{
+loginTextStyle:{
 	fontSize:20,
+  color:'#ffffff',
 },
 alignSelf:{
 	alignSelf:'center',
@@ -60,13 +79,28 @@ color:{
 input:{
 	height:44,
 	width:screen_width,//屏幕的宽度
+	borderBottomWidth:1/PixelRatio.get(),
+	borderBottomColor:'gray',
+  backgroundColor:'#ffffff'
 },
 marginTop:{
-	marginTop:10,
+	marginTop:15,
+},
+lineHeight:{
+  marginTop:2/PixelRatio.get(),
 },
 textCenter:{
   textAlign:'center',
 },
+loginButton:{
+  width:screen_width-30,
+  height:40,
+  backgroundColor:'rgb(0,163,234)',
+  borderRadius:4,
+},
+assistButton:{
+
+}
 });
 
 
